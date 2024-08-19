856b0fa8939bcaab0fb35ca838eaf2b9f11b3f6c
rafshinee <rafaele.00000847840@unicap.br>
2024-08-18 21:57:15 -0300

    aplicação site estático react

---
+++ site-estatico/src/main/java/com/example/site_estatico/SiteEstaticoApplication.java
@@ -0,0 +1,13 @@
+package com.example.site_estatico;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SiteEstaticoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SiteEstaticoApplication.class, args);
+	}
+
+}
---
+++ site-estatico/src/test/java/com/example/site_estatico/SiteEstaticoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.site_estatico;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SiteEstaticoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
